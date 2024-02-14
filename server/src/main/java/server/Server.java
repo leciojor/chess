ed5c34d27c90b55d79c47cc3436d5b1c70f8d48e
leciojor <leciojor@byu.edu>
2024-02-13 20:34:38 -0700

    init phase 3 endpoints set

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -7,9 +7,16 @@ public class Server {
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
-        Spark.staticFiles.location("web");
+        Spark.staticFiles.location("C:\\Users\\lecio\\CS240\\chess\\server\\src\\main\\resources\\web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.delete("/db", (request, response) -> clearGameHandler(request, response));
+        //Spark.post("/user", (request, response) -> registerHandler(request, response));
+        //Spark.post("/session", (request, response) -> loginHandler(request, response));
+        //Spark.delete("/session", (request, response) -> logOutHandler(request, response));
+        //Spark.get("/game", (request, response) -> listGamesHandler(request, response));
+        //Spark.post("/game", (request, response) -> createGameHandler(request, response));
+        //Spark.put("/game", (request, response) -> joinGameHandler(request, response));
 
         Spark.awaitInitialization();
         return Spark.port();
