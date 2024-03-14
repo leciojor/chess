978cc463673a2dfa27d8c19a017f6d2d632619cd
leciojor <leciojor@byu.edu>
2024-03-14 12:32:40 -0600

    phase 5 drawing board

--- client/src/main/java/ui/ChessBoardUi.java
+++ client/src/main/java/ui/ChessBoardUi.java
@@ -1,4 +1,86 @@
 package ui;
 
+import chess.ChessGame;
+import chess.ChessPiece;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
+import static ui.EscapeSequences.*;
+
+
 public class ChessBoardUi {
+
+    private static final int BOARD_SIZE = 30;
+
+
+    public static void main(String[] args) {
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
+        out.print(ERASE_SCREEN);
+
+        drawBox(out);
+    }
+
+
+
+
+
+
+
+
+    private static void drawBox(PrintStream out) {
+        for(int row = 0; row < BOARD_SIZE; row++){
+            for(int col = 0; col < BOARD_SIZE; col++){
+                if (row == 0 || row == BOARD_SIZE - 1) {
+                    out.print(BORDER_UP_DOWN);
+                }
+
+                else if (col == 0){
+                    out.print(BORDER_LEFT);
+                }
+
+                else if (col == BOARD_SIZE - 1){
+                    out.print(BORDER_RIGHT);
+                }
+
+                else {
+                    drawInside(out);
+                }
+
+            }
+            out.println();
+        }
+    }
+
+    private static void drawInside(PrintStream out) {
+
+    }
+
+    private static void setBlack(PrintStream out) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_BLACK);
+    }
+
+    private static void setBlue(PrintStream out) {
+        out.print(SET_BG_COLOR_BLUE);
+        out.print(SET_TEXT_COLOR_BLUE);
+    }
+
+    private static void setWhite(PrintStream out) {
+        out.print(SET_BG_COLOR_WHITE);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+
+    private static void printPlayer(PrintStream out, String player) {
+        out.print(SET_BG_COLOR_WHITE);
+        out.print(SET_TEXT_COLOR_BLACK);
+
+        out.print(player);
+
+        setWhite(out);
+    }
+
+
 }
--- client/src/main/java/ui/EscapeSequences.java
+++ client/src/main/java/ui/EscapeSequences.java
@@ -59,6 +59,15 @@ public class EscapeSequences {
     public static final String BLACK_KNIGHT = " ♞ ";
     public static final String BLACK_ROOK = " ♜ ";
     public static final String BLACK_PAWN = " ♟ ";
+
+    public static final String CIRCLE = "◯";
+
+    public static final String BORDER_LEFT = "╟";
+
+    public static final String BORDER_RIGHT = "╢";
+
+    public static final String BORDER_UP_DOWN = "═";
+
     public static final String EMPTY = " \u2003 ";
 
     public static String moveCursorToLocation(int x, int y) { return UNICODE_ESCAPE + "[" + y + ";" + x + "H"; }
